2e835355a2e94a93c41b379ca3633dafbaecaa90
Daniel Musselwhite <daniel.l.musselwhite@gmail.com>
2022-04-11 20:10:18 +0100

    added collisions for left and right as well as backwards and forwards

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -93,15 +93,26 @@ public class PlayerMovement : MonoBehaviour
             if(col.gameObject.tag == "Wall")
             {
                 Debug.Log(gameObject.name + "collided with wall " + col.gameObject.name);
-                // if we are to the in front of the wall
-                if(transform.position.z < col.transform.position.z)
+                // if we are colliding with the back of the wall, snap to the back of the wall
+                if(transform.position.z < col.bounds.min.z)
                 {
                     transform.position = new Vector3(transform.position.x, transform.position.y, col.transform.position.z - col.transform.localScale.z/2 - pCol.radius);
                 }
-                // if we are behind the wall
-                else if(transform.position.z > col.transform.position.z)
+                // if we are colliding with the front of the wall, snap to the front of the wall
+                else if(transform.position.z > col.bounds.max.z)
                 {
                     transform.position = new Vector3(transform.position.x, transform.position.y, col.transform.position.z + col.transform.localScale.z/2 + pCol.radius);
+                }
+
+                // if we are colliding with the left side of the wall, snap to the left side of the wall
+                if(transform.position.x < col.bounds.min.x)
+                {
+                    transform.position = new Vector3(col.transform.position.x - col.transform.localScale.x/2 - pCol.radius, transform.position.y, transform.position.z);
+                }
+                // if we are colliding with the right side of the wall, snap to the right side of the wall
+                else if(transform.position.x > col.bounds.max.x)
+                {
+                    transform.position = new Vector3(col.transform.position.x + col.transform.localScale.x/2 + pCol.radius, transform.position.y, transform.position.z);
                 }
             }
         }
