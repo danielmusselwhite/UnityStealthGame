24b0d3990dcf94e964bc478ffb5e0ebca339ced7
Daniel Musselwhite <daniel.l.musselwhite@gmail.com>
2022-04-11 20:03:04 +0100

    Improved collision detection so it will slide along wall in the other axis instead of completely stopping if it collide sin one axis

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -13,7 +13,6 @@ public class PlayerMovement : MonoBehaviour
     public float gravity;
 
     private CapsuleCollider pCol;
-    private Vector3 lastSafePosition;
 
 
     // Start is called before the first frame update
@@ -22,13 +21,11 @@ public class PlayerMovement : MonoBehaviour
         speed = walkSpeed;
         direction = 0;
         pCol = GetComponent<CapsuleCollider>();
-        lastSafePosition = transform.position;
     }
 
     // FixedUpdate for physics
     void FixedUpdate()
     {
-        // move the player based on their input
         transform.Translate(0, 0, direction * speed * Time.deltaTime, Space.Self);
         checkCollisions();
     }
@@ -87,46 +84,26 @@ public class PlayerMovement : MonoBehaviour
         //check if we're colliding with something
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, pCol.radius);
 
-        // for each hitCollider
-        for(int i = 0; i < hitColliders.Length; i++){
-            // if this is our hitCollider, skip
-            if(hitColliders[i].gameObject == gameObject)
-                continue;
-
-            Debug.Log(""+gameObject.name+" is colliding with "+hitColliders[i].gameObject.name);
+        // for each collision box we are overlapping
+        foreach(Collider col in hitColliders)
+        {
+           
             
-            Vector3 left = transform.position - transform.right * pCol.radius;
-            Vector3 right = transform.position + transform.right * pCol.radius;
-            // Vector3 forward = transform.position + transform.forward * pCol.radius;
-            // Vector3 back = transform.position - transform.forward * pCol.radius;
-            // Vector3 top = transform.position + transform.up * pCol.height / 2;
-            // Vector3 bottom = transform.position - transform.up * pCol.height / 2;
-
-            //detect the bounds we are colliding with and do not exceed it
-
-
-            if (left.x > hitColliders[i].bounds.min.x)
-                transform.position = new Vector3(hitColliders[i].bounds.max.x + pCol.radius, transform.position.y, transform.position.z);
-            if (right.x < hitColliders[i].bounds.max.x)
-                transform.position = new Vector3(hitColliders[i].bounds.min.x - pCol.radius, transform.position.y, transform.position.z);
-            // if (forward.z > hitColliders[i].bounds.max.z)
-            //     transform.position = new Vector3(transform.position.x, transform.position.y, hitColliders[i].bounds.max.z);
-            // if (back.z < hitColliders[i].bounds.min.z)
-            //     transform.position = new Vector3(transform.position.x, transform.position.y, hitColliders[i].bounds.min.z);
-            // if (top.y > hitColliders[i].bounds.max.y)
-            //     transform.position = new Vector3(transform.position.x, hitColliders[i].bounds.max.y, transform.position.z);
-            // if (bottom.y < hitColliders[i].bounds.min.y)
-            //     transform.position = new Vector3(transform.position.x, hitColliders[i].bounds.min.y, transform.position.z);
+            // if the collision box is a wall
+            if(col.gameObject.tag == "Wall")
+            {
+                Debug.Log(gameObject.name + "collided with wall " + col.gameObject.name);
+                // if we are to the in front of the wall
+                if(transform.position.z < col.transform.position.z)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y, col.transform.position.z - col.transform.localScale.z/2 - pCol.radius);
+                }
+                // if we are behind the wall
+                else if(transform.position.z > col.transform.position.z)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y, col.transform.position.z + col.transform.localScale.z/2 + pCol.radius);
+                }
+            }
         }
-
-        //check if we are colliding to the right of the x
-
-
-        //if we're colliding with something, move us back to the last safe position
-        // if (hitColliders.Length > 1)
-        //     transform.position= new Vector3(lastSafePosition.x, lastSafePosition.y, transform.position.z);
-        // //we're not colliding with a wall, save our position
-        // else
-        //     lastSafePosition = transform.position;
     }
 }
