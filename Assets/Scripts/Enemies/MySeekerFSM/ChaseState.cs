93fa3570c8cae7e7d47b5e5c4e4928dff3dd1912
Daniel Musselwhite <daniel.l.musselwhite@gmail.com>
2022-04-18 22:38:33 +0100

    Chase and Patrol State gradually rotate to look at player

--- Assets/Scripts/Enemies/MySeekerFSM/ChaseState.cs
+++ Assets/Scripts/Enemies/MySeekerFSM/ChaseState.cs
@@ -7,6 +7,7 @@ namespace Enemies.MySeekerFSM{
     public class ChaseState : State
     {
 
+        public float turnSpeed = 180f; // rotate 180 degrees per second
         public float speed = 5f;
         private GameObject player;
         
@@ -25,8 +26,9 @@ namespace Enemies.MySeekerFSM{
             // replace with A* later
             if(player != null)
             {
-                transform.LookAt(player.transform); // look at the player
-                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime); // move towards the player
+                Vector3 targetPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z); // set the target position to the player's position
+                TurnToFace(player.transform.position); // look at the player
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime); // move towards the player
             }
 
             if (Vector3.Distance(transform.position, player.transform.position) > 10f){ // if the player is exceeding a threshold of units of the enemy, switch to PatrolState
@@ -38,5 +40,25 @@ namespace Enemies.MySeekerFSM{
         {
             Debug.Log("FSM | Exited Chase State");
         }
+
+        #region "Movement methods"
+        private void TurnToFace(Vector3 lookTarget){
+            // get the direction to the target
+            Vector3 directionToTarget = (lookTarget - transform.position).normalized;
+            // get the angle between the direction to the target and the direction the enemy is facing
+            float targetAngle = 90 - Mathf.Atan2(directionToTarget.z, directionToTarget.x) * Mathf.Rad2Deg;
+
+             if(Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle)) <= 0.05f){ // if we are in a threshold snap to it, set the angle to the target angle
+                transform.eulerAngles = Vector3.up * targetAngle; 
+                return; // and return as we don't want to keep rotating
+            }
+            // else, turn the enemy to face the target
+            else{
+                float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
+                transform.eulerAngles = Vector3.up * angle;
+            }
+
+        }
+        #endregion
     }
 }
--- Assets/Scripts/Enemies/MySeekerFSM/PatrolState.cs
+++ Assets/Scripts/Enemies/MySeekerFSM/PatrolState.cs
@@ -7,6 +7,7 @@ namespace Enemies.MySeekerFSM{
     public class PatrolState : State
     {
 
+        public float turnSpeed = 90f; // rotate 90 degrees per second
         public float speed = 5f;
         private GameObject player;
 
@@ -23,11 +24,34 @@ namespace Enemies.MySeekerFSM{
             if(Vector3.Distance(transform.position, player.transform.position) < 10f){ // if the player is within a threshold of units of the enemy, switch to ChaseState
                 SM.ChangeState(new ChaseState());
             }
+
+            TurnToFace(player.transform.position);
+            
         }
 
         public override void Exit()
         {
             Debug.Log("FSM | Exited Patrol State");
         }
+
+        #region "Movement methods"
+        private void TurnToFace(Vector3 lookTarget){
+            // get the direction to the target
+            Vector3 directionToTarget = (lookTarget - transform.position).normalized;
+            // get the angle between the direction to the target and the direction the enemy is facing
+            float targetAngle = 90 - Mathf.Atan2(directionToTarget.z, directionToTarget.x) * Mathf.Rad2Deg;
+
+             if(Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle)) <= 0.05f){ // if we are in a threshold snap to it, set the angle to the target angle
+                transform.eulerAngles = Vector3.up * targetAngle; 
+                return; // and return as we don't want to keep rotating
+            }
+            // else, turn the enemy to face the target
+            else{
+                float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
+                transform.eulerAngles = Vector3.up * angle;
+            }
+
+        }
+        #endregion
     }
 }
